251f918fa45fb8e99c4bd91a78414993cdff035d
tssiu5 <tssiu5@vps29.cs.cityu.edu.hk>
2019-11-09 16:28:20 +0800

    Tim Siu

--- pcmaker/src/gui/DIYTab.java
+++ pcmaker/src/gui/DIYTab.java
@@ -3,21 +3,33 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Enumeration;
 
 import javax.swing.*;
 import javax.swing.event.*;
 
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import backend.CPUController;
+import backend.Controller;
+import backend.SpecList;
+
 public class DIYTab extends JPanel implements TotalObserver{
 	//private JTextField totalBudget;
-	private JLabel totalBudgetlabel, checkSubmittedTotalBudgetLabel;
+	private JLabel totalBudgetlabel, checkSubmittedTotalBudgetLabel,resultOutput;
 	private JButton calculateButtton;
 	private double totalBudgetValue = 0;
 	private Budget budget;
+	private SpecList specList;
 	private ButtonGroup cpuGroup, cardGroup;
+	private boolean calRsult = false;
 	
-	public DIYTab(){
-		budget = new Budget(this);    
+	public DIYTab() throws IOException{
+		budget = new Budget(this);
 	    //totalBudget = new JTextField("0",10);
         Component cpuBar = new Component_CPU(this, "CPU",budget);
         Component motherboardBar = new Component_Motherboard(this, "Motherboard",budget);
@@ -25,10 +37,17 @@ public class DIYTab extends JPanel implements TotalObserver{
         Component videoBar = new Component_VideoCard(this, "Video Card", budget);
         Component psuBar = new Component_PowerSupply(this, "Power Supply",budget);
         Component storageBar = new Component_Storage(this, "Storage",budget);
-        
         //totalBudget.setBounds(10, 50, 200, 25);
         //this.add(totalBudget);
         
+        FileInputStream file = new FileInputStream(new File("Components.xlsx"));
+		XSSFWorkbook workbook = new XSSFWorkbook(file);
+		CPUController cpuController = new CPUController(workbook);
+
+        JLabel specListOutput = new JLabel();
+    	specListOutput.setText("The following specification list is fulfilled your requirement:");
+    	specListOutput.setBounds(400, 0, 500, 60);
+    	this.add(specListOutput);
         /*totalBudget.getDocument().addDocumentListener(new DocumentListener() {
         	public void changedUpdate(DocumentEvent e) {
         	   warn();
@@ -84,7 +103,7 @@ public class DIYTab extends JPanel implements TotalObserver{
         this.add(totalBudgetlabel);*/
 
         checkSubmittedTotalBudgetLabel= new JLabel();
-        checkSubmittedTotalBudgetLabel.setText("Total Budget: $0");
+        checkSubmittedTotalBudgetLabel.setText("Total Budget: $ 0");
         checkSubmittedTotalBudgetLabel.setBounds(10, 220, 180, 50);
         this.add(checkSubmittedTotalBudgetLabel);
         
@@ -98,9 +117,10 @@ public class DIYTab extends JPanel implements TotalObserver{
         		budget.setCPUPreference(getSelectedButtonText(cpuGroup));
         		budget.setCardPreference(getSelectedButtonText(cardGroup));
         		budget.showTotal();
+        		specList = new SpecList(budget,cpuController);
+        		specList.print();
 			}          
 	      });
-        
         
         JLabel cpuBrand = new JLabel("CPU Brand Preference");
         cpuBrand.setBounds(10, 270, 180, 50);
@@ -136,17 +156,16 @@ public class DIYTab extends JPanel implements TotalObserver{
         cardGroup.add(nvidiaCard);
         cardGroup.add(amdCard);
         cardGroup.add(npCard);
-        
-
-        
         this.setLayout(null);
 	}
 	
 	public void updateTotal() {
-		checkSubmittedTotalBudgetLabel.setText("Total Budget: $" + budget.getTotal());
+		checkSubmittedTotalBudgetLabel.setText("Total Budget: $ " + budget.getTotal());
 	}
 	
-
+	public void refreshresult() {
+		this.add(resultOutput);
+	}
 	public String getSelectedButtonText(ButtonGroup buttonGroup) {
 	    for (Enumeration<AbstractButton> buttons = buttonGroup.getElements(); buttons.hasMoreElements();) {
 	        AbstractButton button = buttons.nextElement();
